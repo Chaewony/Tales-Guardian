cbd9f85789e66de01f6197c940f934faaca6869a
Chaewony <tcd1205@naver.com>
2021-07-06 17:21:05 +0900

    7/6

---
+++ Talse Guardian 3D/Assets/Scripts/Button.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour
+{
+    public ButtonType buttonType;
+    public GameObject selectCharUI;
+    public Character character;
+    // Start is called before the first frame update
+
+    public void OnButtonClick()
+	{
+		switch (buttonType)
+		{
+            case ButtonType.Settings:
+                Debug.Log("Settings 클릭");
+                break;
+            case ButtonType.Library:
+                Debug.Log("Library 클릭");
+                break;
+            case ButtonType.Arrangement:
+                Debug.Log("Arrangement 클릭");
+                break;
+            case ButtonType.Theme:
+                Debug.Log("Theme 클릭");
+                break;
+            case ButtonType.SelectChar:
+                Debug.Log("SelectChar 클릭");
+                character.ShowCharacters();
+                selectCharUI.SetActive(true);
+                break;
+            case ButtonType.Close:
+                Debug.Log("창닫기 클릭");
+                selectCharUI.SetActive(false);
+                break;
+            case ButtonType.Char0:
+                character.SelectMainCharacter(0);
+                break;
+            case ButtonType.Char1:
+                character.SelectMainCharacter(1);
+                break;
+            case ButtonType.Char2:
+                character.SelectMainCharacter(2);
+                break;
+            case ButtonType.Char3:
+                character.SelectMainCharacter(3);
+                break;
+        }
+	}
+}
---
+++ Talse Guardian 3D/Assets/Scripts/CharacterInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+[CreateAssetMenu(menuName ="Character", fileName ="Character/CharacterInfo")]
+
+public class CharacterInfo : ScriptableObject
+{
+    [SerializeField]
+    private Sprite sprite;
+    [SerializeField]
+    private Sprite illustSprite;
+    [SerializeField]
+    private string charName;
+    [SerializeField]
+    private float fullHp;
+    [SerializeField]
+    private float fullAtk;
+    [SerializeField]
+    private float fullAr; //Armor 물리방어력
+    [SerializeField]
+    private float fullMr; //Magic resist 마법방어력
+    [SerializeField]
+    private bool isOwning; //플레이어가 가지고 있는 카드인지
+
+    public Sprite mySprite { get => sprite; }
+    public Sprite myIllustSprite { get => illustSprite; }
+    public string myCharName { get => charName; }
+    public float myHp { get => fullHp; }
+    public float myAtk { get => fullAtk; }
+    public float myAr { get => fullAr; }
+    public float myMr { get => fullMr; }
+    public bool myIsOwning { get => isOwning; }
+}
