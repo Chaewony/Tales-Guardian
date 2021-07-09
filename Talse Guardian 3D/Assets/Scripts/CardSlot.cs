abea7e8ddd6a1ce70f98271eea3e9676caa3c5d2
Chaewony <tcd1205@naver.com>
2021-07-09 17:43:53 +0900

    7/9

--- Talse Guardian 3D/Assets/Scripts/CardSlot.cs
+++ Talse Guardian 3D/Assets/Scripts/CardSlot.cs
@@ -17,6 +17,7 @@ public class CardSlot : MonoBehaviour, IPointerClickHandler
 	private Text FirstSkillName;
 	[SerializeField]
 	private Text SecondSkillName;
+	public GameManager gameManager;
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
@@ -25,6 +26,7 @@ public class CardSlot : MonoBehaviour, IPointerClickHandler
 		PositionType.text = "포지션 타입: " + character.myPosType;
 		FirstSkillName.text = "스킬1: " + character.myFirstSkillName;
 		SecondSkillName.text = "스킬2: " + character.mySecondSkillName;
-		character.myIsSelected = true;
+		gameManager.SetAllCharactersToSelectedFalse();//미리 selected 되어있던 캐릭터의 isSelected는 꺼주기
+		character.myIsSelected = true; 
 	}
 }
--- Talse Guardian 3D/Assets/Scripts/SelectGrid.cs
+++ Talse Guardian 3D/Assets/Scripts/SelectGrid.cs
@@ -10,7 +10,8 @@ public class SelectGrid : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 	public Image gridColor;
 	Vector3 defaultScale;
 	Color defaultColor;
-	private bool isClicked;
+	public bool isClicked; //이 스크립트가 부착된 그리드(한 칸)이 클릭된 상태인지 아닌지 판별
+	public GameManager gameManager;
 	private void Start()
 	{
 		defaultScale = gridScale.localScale;
@@ -24,6 +25,7 @@ public class SelectGrid : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 			gridScale.localScale = defaultScale * 1.05f;
 			gridColor.color = new Color(gridColor.color.r, 0.25f, gridColor.color.b);
 		}
+		else return;
 	}
 
 	public void OnPointerExit(PointerEventData eventData)
@@ -38,15 +40,30 @@ public class SelectGrid : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
 	public void OnPointerClick(PointerEventData eventData)
 	{
-		if (!isClicked)
+		//클릭되지 않은 상태의 그리드를 클릭했을 때
+		if (!isClicked&&gameManager.canClickGrid)
 		{
+			//그리드 선택
 			isClicked = true;
-			gridColor.color = new Color(gridColor.color.r, 0.25f, gridColor.color.b);
+			//gridColor.color = new Color(gridColor.color.r, 0.25f, gridColor.color.b);
+			gridColor.color = new Color(1f, 1f, 1f);
+			//캐릭터 리스트를 돌면서 isSelected가 true로 되어있는 캐릭터의 정보를 가져온다
+			for (int i = 0; i < gameManager.allCharacter.Count; i++) {
+				if (gameManager.allCharacter[i].myIsSelected)
+				{
+					gridColor.sprite = gameManager.allCharacter[i].mybattleSprite;
+					//gridColor.color = defaultColor;
+				}
+			}
+			gameManager.SetAllCharactersToSelectedFalse();
 		}
-		else
+		//클릭된 상태의 그리드를 클릭했을 때 (그리드 선택 해제는 언제든지 가능)
+		else if(isClicked)
 		{
+			//그리드 선택 해제
 			isClicked = false;
 			gridColor.color = defaultColor;
+			gridColor.sprite = null; //재클릭시 이미지 빼기(선택 해제)
 		}
 	}
 }
