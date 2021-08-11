deba2afc5a89a039d5439e9efc57c135733fc74d
Chaewony <tcd1205@naver.com>
2021-08-12 01:48:42 +0900

    8/12

---
+++ Tales Guardian 3D/Assets/Scripts/Battle/PlayerSquadUI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSquadUI : MonoBehaviour
+{
+    [SerializeField]
+    private BattleManager battleManager;
+    [SerializeField]
+    private Image[] charImageUI;
+
+    public Text[] hpValue;
+    public Image[] hpBar;
+
+    [SerializeField]
+    private Text[] skill1;
+    [SerializeField]
+    private Text[] skill2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShowCharImageUI();
+        ShowHpBar();
+        ShowSkill();
+    }
+
+    void ShowCharImageUI()
+	{
+        for (int i = 0; i < battleManager.playerSquad.Count; i++)
+            charImageUI[i].sprite = battleManager.playerSquad[i].mySprite;
+    }
+    void ShowHpBar()
+	{
+        for (int i = 0; i < battleManager.playerSquad.Count; i++)
+        {
+            hpValue[i].text = battleManager.playerSquad[i].myCurrentHp + "/" + battleManager.playerSquad[i].myHp;
+            hpBar[i].fillAmount = battleManager.playerSquad[i].myCurrentHp / battleManager.playerSquad[i].myHp;
+            if (battleManager.playerSquad[i].myCurrentHp < 0)
+            {
+                battleManager.playerSquad[i].myCurrentHp = 0;
+            }
+        }
+    }
+    void ShowSkill()
+	{
+        //스킬1
+        for (int i = 0; i < battleManager.playerSquad.Count; i++)
+        {
+            skill1[i].text = battleManager.playerSquad[i].myFirstSkillName;
+        }
+        //스킬2
+        for (int i = 0; i < battleManager.playerSquad.Count; i++)
+        {
+            skill2[i].text = battleManager.playerSquad[i].mySecondSkillName;
+        }
+    }
+}
---
+++ Tales Guardian 3D/Assets/Scripts/EnemyArrangement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyArrangement : MonoBehaviour
+{
+    // 에너미 인포들
+    public List<EnemyInfo> allEnemy = new List<EnemyInfo>(); //스크립터블 오브젝트
+    // 에너미 필드
+    public Image[] EnemyFields;
+    // 스테이지 인포
+    public List<StageInfo> allStage = new List<StageInfo>(); //스크립터블 오브젝트
+
+    int stageIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CheckStage();
+        SetImage();
+    }
+
+    void CheckStage()
+    {
+        for(int i = 0; i < allStage.Count; i++)
+        {
+            if (allStage[i].isSelected)
+            { stageIndex = i; }
+        }
+    }
+    void SetImage() //에너미 필드의 이미지를 바꿔줌
+    {
+        for(int i=0;i< allStage[stageIndex].EnemyIndex.Length;i++)
+        {
+            EnemyFields[allStage[stageIndex].EnemyArrangedIndex[i]].color = new Color(1, 1, 1);
+            EnemyFields[allStage[stageIndex].EnemyArrangedIndex[i]].sprite = allEnemy[allStage[stageIndex].EnemyIndex[i]].mySprite;
+        }
+    }
+
+}
