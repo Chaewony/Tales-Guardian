2505a35565d2e230eb15768617f1c8c19c65f692
Chaewony <tcd1205@naver.com>
2021-08-23 18:53:01 +0900

    8/23

---
+++ TalesGuardian/Tales Guardian 3D/Assets/Scripts/Info/FieldInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "Field", fileName = "Field/FieldInfo")]
+
+public class FieldInfo : ScriptableObject
+{
+    [SerializeField]
+    int index;
+    [SerializeField]
+    int arrangedCharIndex;
+    public int myIndex { get => index; }
+    public int myArrangedCharIndex { get => arrangedCharIndex; set => arrangedCharIndex = value; }
+}
