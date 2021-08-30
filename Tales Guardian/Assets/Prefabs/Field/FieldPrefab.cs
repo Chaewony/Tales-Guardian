d4a00438082554366bf57078a08a7d72afa07ca8
vliza321 <82195937+vliza321@users.noreply.github.com>
2021-08-30 14:38:28 +0900

    재업로드

---
+++ Tales Guardian/Assets/Prefabs/Field/FieldPrefab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldPrefab : MonoBehaviour
+{
+    [SerializeField]
+    int index;
+    [SerializeField]
+    public int arrangedCharIndex;
+
+    public int myIndex { get => index; }
+    public int myArrangedCharIndex { get => arrangedCharIndex; set => arrangedCharIndex = value; }
+}
