6c6ce56537946992144c69f86a806f7739dda140
vliza321 <82195937+vliza321@users.noreply.github.com>
2021-09-06 22:31:31 +0900

    이것저것 많이 바뀜

--- Tales Guardian/Assets/Prefabs/UserInformation.cs
+++ Tales Guardian/Assets/Prefabs/UserInformation.cs
@@ -9,5 +9,6 @@ public class UserInformation : MonoBehaviour
     [SerializeField]
     private string UserName;
 
+    public string MyUserName { get => UserName; set => UserName = value; }
     public int MyHaveBookIndx { get => HaveBookIndex; set => HaveBookIndex = value; }
 }
