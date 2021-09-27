5b942f607a2a2f287f570a46ccb215aa411540ab
vliza321 <82195937+vliza321@users.noreply.github.com>
2021-09-27 14:52:29 +0900

    9/27
    결과 씬 베이스

---
+++ Tales Guardian/Assets/Scripts/ResultScene/GameResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResult : MonoBehaviour
+{
+    public bool IsWin;
+
+    public void Start()
+    {
+        IsWin = true;
+    }
+}
---
+++ Tales Guardian/Assets/Scripts/ResultScene/GameResultManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameResultManager : MonoBehaviour
+{
+    public bool BattleResult;
+    public GameObject BattleSceneResult;
+    public Text ResultText;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        BattleSceneResult = GameObject.Find("GameResult").gameObject;
+        BattleResult = BattleSceneResult.GetComponent<GameResult>().IsWin;
+        SetResult();
+    }
+
+    private void SetResult()
+    {
+        if(BattleResult)
+        {
+            ResultText.text = "이겼음 수고링";
+        }
+        else
+        {
+            ResultText.text = "졌음 수고링";
+        }
+    }
+
+    private void OnDisable()
+    {
+        Destroy(GameObject.Find("GameResult").gameObject); 
+    }
+}
