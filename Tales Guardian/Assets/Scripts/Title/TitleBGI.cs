6776f58951d8b7673fa53e8db3b673382c4865bb
vliza321 <82195937+vliza321@users.noreply.github.com>
2021-10-01 01:49:18 +0900

    10/1
    이것 저것

---
+++ Tales Guardian/Assets/Scripts/Title/TitleBGI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleBGI : MonoBehaviour
+{
+    public GameObject BGI1;
+    public GameObject BGI2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a = Random.Range(0,2);
+        if(a ==0)
+        {
+            BGI1.SetActive(true);
+            BGI2.SetActive(false);
+        }
+        if (a == 1)
+        {
+            BGI1.SetActive(false);
+            BGI2.SetActive(true);
+        }
+    }
+}
